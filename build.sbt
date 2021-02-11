509dcdd3cc4d1f77742826588f87bd0d01ffc9db
mkuznetsov-isg <mkuznetsov@isgneuro.com>
2021-02-11 16:06:43 +0300

    Test embedded in Spark log4j_v1

--- build.sbt
+++ build.sbt
@@ -6,4 +6,6 @@ scalaVersion := "2.11.12"
 
 //libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.14.0" % Runtime
 
-libraryDependencies += "org.apache.logging.log4j" %% "log4j-api-scala" % "11.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.3"
+
+//libraryDependencies += "org.apache.logging.log4j" %% "log4j-api-scala" % "11.0"
--- src/main/scala/MyClass.scala
+++ src/main/scala/MyClass.scala
@@ -1,39 +1,57 @@
-import org.apache.logging.log4j.scala.Logging
-import org.apache.logging.log4j.Level
+//import org.apache.logging.log4j.scala.Logging
+//import org.apache.logging.log4j.Level
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
 
 class User(val name: String)
 
-class MyClass extends Logging {
+class MyClass extends /*Logging*/ {
 
-  def doStuff(): Unit =
-    logger.info("Doing stuff")
+//  def doStuff(): Unit =
+//    logger.info("Doing stuff")
+//
+//  def doStuffWithLevel(level: Level): Unit =
+//    logger(level, "Doing stuff with arbitrary level")
+//
+//  def doStuffWithUser(user: User): Unit =
+//    logger.info(s"Doing stuff with ${user.name}")
 
-  def doStuffWithLevel(level: Level): Unit =
-    logger(level, "Doing stuff with arbitrary level")
+  val spark: SparkSession = SparkSession
+    .builder
+    .appName("embedded log4j test")
+    .master("local[*]")
+    .getOrCreate()
+  println("spark started")
 
-  def doStuffWithUser(user: User): Unit =
-    logger.info(s"Doing stuff with ${user.name}")
+  val log: Logger = Logger.getLogger("logger")
+  log.fatal("111")
+
+  spark.stop
+  println("spark shutdowned")
+
 
 }
 
 object MyObj extends App {
 
   val mc = new MyClass
-  mc.doStuff()
-  println()
-
-  // *
-  mc.doStuffWithLevel(Level.ALL)
-  // *
-  mc.doStuffWithLevel(Level.DEBUG)
-  mc.doStuffWithLevel(Level.ERROR)
-  mc.doStuffWithLevel(Level.FATAL)
-  mc.doStuffWithLevel(Level.INFO)
-  // *
-  mc.doStuffWithLevel(Level.TRACE)
-  mc.doStuffWithLevel(Level.WARN)
-  println()
-
-  mc.doStuffWithUser(new User("mike"))
+//  mc.doStuff()
+//  println()
+//
+//  // *
+//  mc.doStuffWithLevel(Level.ALL)
+//  // *
+//  mc.doStuffWithLevel(Level.DEBUG)
+//  mc.doStuffWithLevel(Level.ERROR)
+//  mc.doStuffWithLevel(Level.FATAL)
+//  mc.doStuffWithLevel(Level.INFO)
+//  // *
+//  mc.doStuffWithLevel(Level.TRACE)
+//  mc.doStuffWithLevel(Level.WARN)
+//  println()
+//
+//  mc.doStuffWithUser(new User("mike"))
 
 }
